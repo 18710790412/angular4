637acf9985d7d87e88f2f2a29ffdfa5cd7fb0401
18710790412@163.com <hxx421924>
2020-03-25 14:43:06 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: MenuAddComponent },
+  { path: 'detail/:id', component: MenuDetailComponent },
+  { path: 'home', component: EmptyComponent ，data:{title:'测试1'}},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  //空目录，重定向处理
+  { path: '**', component: EmptyComponent } //通配符，其他路由失败的，会跳转到这个视图
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
